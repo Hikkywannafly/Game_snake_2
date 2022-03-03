c50db5fa8f26dd0de106b42e0c38b01bdf30f945
ggraygon@gmail.com <ggraygon@gmail.com>
2022-03-03 14:57:48 +0700

    upload1

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 
-import static java.lang.Thread.sleep;
-
 public class GameFrame extends JPanel implements Runnable {
     int [] [] bg = new int[20][20];
     Thread thread;
@@ -23,10 +21,10 @@ public class GameFrame extends JPanel implements Runnable {
 
         for(int i =0; i<20; i++){
             for(int j = 0; j<20; j++){
-                if(bg[i][j] == 0) g.setColor(Color.gray);
-                if(bg[i][j] == 1) g.setColor(Color.GREEN);
-                if(bg[i][j] == 2) g.setColor(Color.RED);
-                g.fillRect(i*20 +2 ,j*20 +2,18,18);
+               if(bg[i][j] == 0) g.setColor(Color.white);// luoi
+              //  if(bg[i][j] == 1) g.setColor(Color.GREEN);//con ran
+                if(bg[i][j] == 2) g.setColor(Color.RED);//moi
+                g.fillRect(i*20+2  ,j*20+2,20,20);
             }
         }
     }
@@ -34,10 +32,10 @@ public class GameFrame extends JPanel implements Runnable {
     public void run(){
         while(true){
 
-            snake.update();
+            snake.move();
             repaint();
             try {
-                sleep(20);
+                thread.sleep(20);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/Snake.java
+++ src/Snake.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 
 public class Snake {
-    int lenghtS = 1;
+    int lenghtS = 4;
 
     int x [];
     int y [];
@@ -18,7 +18,12 @@ public class Snake {
 
         x[0] = 5;
         y[0] = 4;
-
+        x[1] = 5;
+        y[1] = 3;
+        x[2] = 5;
+        y[2] = 2;
+        x[3] = 5;
+        y[3] = 1;
     }
     public void setVector(int v){
         if(vector != -v){
@@ -26,8 +31,17 @@ public class Snake {
         }
 
     }
-    public void update(){
+    public void move(){
+
         if(System.currentTimeMillis() - t1 >500){
+            for(int i = lenghtS - 1 ; i>0; i--){
+                x[i] = x[i-1];
+                y[i] = y[i-1];
+            }
+//            for(int i = 1 ; i <= lenghtS; i++){
+//                x[i] = x[i-2];
+//                y[i] = y[i-2];
+//            }
            if( vector == Snake.GO_UP) y[0]--;
             if( vector == Snake.GO_DOWN) y[0]++;
             if( vector == Snake.GO_LEFT) x[0]--;
@@ -37,9 +51,13 @@ public class Snake {
 
     }
     public void paintSnake(Graphics g){
-        g.setColor(Color.GREEN);
+
         for(int i = 0; i<lenghtS; i++){
-            g.fillRect(x[i]*20 +2 ,y[i]*20 +2,18,18);
+            if( i == 0) g.setColor(new Color(0 ,205 ,0));
+            else g.setColor(new Color(0, 255, 127));
+
+                g.fillRect(x[i]*20 +2 ,y[i]*20 +2,20,20);
+
         }
     }
 }
